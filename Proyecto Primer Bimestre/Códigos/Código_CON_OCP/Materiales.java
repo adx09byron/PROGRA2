9461e33dd4a1f5023f2b8c29c9e78ff92c3dc02d
Luis <pachecoluis7280@gmail.com>
2025-06-12 14:46:17 -0500

    Update Materiales.java

--- Proyecto Primer Bimestre/Códigos/Código_CON_OCP/Materiales.java
+++ Proyecto Primer Bimestre/Códigos/Código_CON_OCP/Materiales.java
@@ -1,8 +1,8 @@
 package org.example;
+
 public interface Materiales {
     String getId();
     String getTitulo();
-    String getAutor();
     boolean isHabilitado();
     void setHabilitado(boolean habilitado);
     void mostrarMaterial();
