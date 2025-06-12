d796c4cef51e52eeae0bdf59fba9599b797190b2
Luis <pachecoluis7280@gmail.com>
2025-06-12 14:50:08 -0500

    Update DvD.java

--- Proyecto Primer Bimestre/Códigos/Código_CON_OCP/DvD.java
+++ Proyecto Primer Bimestre/Códigos/Código_CON_OCP/DvD.java
@@ -1,45 +1,42 @@
 package org.example;
 
-public class DvD implements Materiales{
+public class DvD implements Material {
     private String id;
     private String titulo;
     private String autor;
     private String genero;
     private String formato;
     private boolean habilitado;
-    public DvD(String id, String titulo, String autor, String genero, String formato){
+
+    public DvD(String id, String titulo, String autor, String genero, String formato) {
         this.id = id;
         this.titulo = titulo;
         this.autor = autor;
-        this.habilitado = true;
         this.genero = genero;
         this.formato = formato;
+        this.habilitado = true;
     }
+
     @Override
     public String getId() {
         return id;
     }
-
     @Override
     public String getTitulo() {
         return titulo;
     }
-
-    @Override
-    public String getAutor() {
-        return autor;
-    }
-
     @Override
     public boolean isHabilitado() {
         return habilitado;
     }
-
     @Override
     public void setHabilitado(boolean habilitado) {
         this.habilitado = habilitado;
     }
-    public  void mostrarMaterial(){
-        System.out.println("Dvd: " + titulo + "\nAutor: " + autor + "\nId: " + id + "\nDisponivilidad: " + "\nGenero: " + genero + (habilitado ? "Si" : "No"));
+
+    @Override
+    public void mostrarMaterial() {
+        System.out.printf("DVD: %s, ID: %s, Autor: %s, Género: %s, Formato: %s, Habilitado: %b%n",
+                titulo, id, autor, genero, formato, habilitado);
     }
 }
