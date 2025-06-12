de85e4f3ee1800330786bd215911b1253425bdc4
Luis <pachecoluis7280@gmail.com>
2025-06-12 14:48:45 -0500

    Update Visitante.java

--- Proyecto Primer Bimestre/Códigos/Código_CON_OCP/Visitante.java
+++ Proyecto Primer Bimestre/Códigos/Código_CON_OCP/Visitante.java
@@ -1,45 +1,62 @@
 package org.example;
 
-public class Visitante extends Usuario {
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Visitante implements Usuario, PrestamoStrategy {
+    private String nombre;
+    private String apellido;
+    private int idBiblioteca;
+    private int edad;
+    private String correo;
     private String ciudad;
     private boolean extranjero;
+    private ArrayList<Prestamo> prestamosActivos;
 
-    public Visitante(String nombre, String apellido, int idBiblioteca, int edad, String correoElectronico, String ciudad, boolean extranjero) {
-        super(nombre, apellido, idBiblioteca, edad, correoElectronico);
-        this.setCiudad(ciudad);
-        this.setExtranjero(extranjero);
+    public Visitante(String nombre, String apellido, int idBiblioteca, int edad, String correo, String ciudad, boolean extranjero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.idBiblioteca = idBiblioteca;
+        this.edad = edad;
+        this.correo = correo;
+        this.ciudad = ciudad;
+        this.extranjero = extranjero;
+        this.prestamosActivos = new ArrayList<>();
+    }
+
+    @Override
+    public int getIdBiblioteca(){ 
+        return idBiblioteca; 
+    }
+    @Override
+    public String getNombre(){ 
+        return nombre + " " + apellido;
+    }
+    @Override
+    public boolean puedePrestar(){ 
+        return prestamosActivos.size() < 1;
+    }
+    @Override
+    public int getDiasMaximoPrestamo(){ 
+        return 3; 
+    }
+    @Override
+    public ArrayList<Prestamo> getPrestamosActivos(){ 
+        return prestamosActivos; 
     }
 
     @Override
     public void mostrarCliente() {
-        System.out.println("Visitante: " + getNombre() + " " + getApellido() + "\nID: V" + getIdBiblioteca() + "\nEdad: " + getEdad() +
-                "\nCorreo: " + getCorreoElectronico() + "\nCiudad: " + getCiudad() + "\nExtranjero: " + (isExtranjero() ? "Sí" : "No"));
+        System.out.printf("Visitante: %s, ID: %d, Edad: %d, Correo: %s, Ciudad: %s, Extranjero: %b%n",
+                getNombre(), idBiblioteca, edad, correo, ciudad, extranjero);
     }
 
     @Override
-    public void mostrarDeudas() {
-
-    }
-
-    @Override
-    public int getDiasMaximoPrestamo() {
-        return 3;
-    }
-
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public boolean isExtranjero() {
-        return extranjero;
-    }
-
-    public void setExtranjero(boolean extranjero) {
-        this.extranjero = extranjero;
+    public double calcularMulta(LocalDate fechaDevolucion, LocalDate fechaActual) {
+        if (fechaActual.isAfter(fechaDevolucion)) {
+            long diasRetraso = fechaActual.toEpochDay() - fechaDevolucion.toEpochDay();
+            return diasRetraso * 5.0; // Multa de $5 por día de retraso
+        }
+        return 0.0;
     }
 }
